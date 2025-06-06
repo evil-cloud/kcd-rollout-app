276c0c03c7e99779b99fec685edac62950210d45
Jose Reynoso <reynosojose2005@gmail.com>
2025-06-06 16:29:55 -0600

    vie 06 jun 2025 16:29:55 CST

--- src/core/config.py
+++ src/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "GitOps API"
     VERSION: str = "1.0.2"
     # ROLLOUT_STRATEGY: str = "v1.0.3" ## Antigua versión
-    ROLLOUT_STRATEGY: str = "v1.0.4"   ## Nueva versión
+    ROLLOUT_STRATEGY: str = "v1.0.5"   ## Nueva versión
     EXTERNAL_API_URL: str = "https://jsonplaceholder.typicode.com/todos/1"
 
 settings = Settings()
