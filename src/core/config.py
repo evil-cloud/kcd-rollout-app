997cd918548c55d79dc02062a2e54478f63d85e4
Jose Reynoso <reynosojose2005@gmail.com>
2025-06-11 08:12:07 -0600

    mié 11 jun 2025 08:12:07 CST

--- src/core/config.py
+++ src/core/config.py
@@ -4,8 +4,8 @@ import os
 class Settings(BaseSettings):
     PROJECT_NAME: str = "GitOps API"
     VERSION: str = "1.0.2"
-    ROLLOUT_STRATEGY: str = "v1.0.3"
-    ROLLOUT_MESSAGE:  str = "¡Hola mundo desde Argo Rollout!"
+    ROLLOUT_STRATEGY: str = "v1.0.4"
+    ROLLOUT_MESSAGE:  str = "¡Hola mundo desde Argo, hola KCD!"
     EXTERNAL_API_URL: str = "https://jsonplaceholder.typicode.com/todos/1"
 
 settings = Settings()
