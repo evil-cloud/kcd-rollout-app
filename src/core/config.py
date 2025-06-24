bb94fa460c33ded9256bd5d9a96c1802829fb834
Jose Reynoso <reynosojose2005@gmail.com>
2025-06-24 01:18:06 -0600

    ops: update message

--- src/core/config.py
+++ src/core/config.py
@@ -4,8 +4,8 @@ import os
 class Settings(BaseSettings):
     PROJECT_NAME: str = "GitOps API"
     VERSION: str = "1.0.2"
-    ROLLOUT_STRATEGY: str = "v1.0.1"
-    ROLLOUT_MESSAGE:  str = "¡Hola mundo desde Argo Rollouts, hola Kubernetes Community Day!"
+    ROLLOUT_STRATEGY: str = "v1.0.2"
+    ROLLOUT_MESSAGE:  str = "¡Hola mundo desde Argo Rollouts, hola Kubernetes Community Day Guatemala, veamos GitOps!"
     EXTERNAL_API_URL: str = "https://jsonplaceholder.typicode.com/todos/1"
 
 settings = Settings()
